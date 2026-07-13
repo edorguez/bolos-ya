f67ec16aa3c67b62d6edad09494c4bd0eec91959
edorguez <edro998@gmail.com>
2026-07-12 20:26:08 -0400

    fix githuv actions

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -5,6 +5,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 config.resolver = config.resolver || {};
+config.resolver.assetExts = [...(config.resolver.assetExts || []), 'wasm'];
 config.resolver.platforms = ['ios', 'android', 'web'];
 
 module.exports = config;
